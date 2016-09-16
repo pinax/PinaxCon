b58ddb60cced2952f4d07321437a5477cabcbd03
Clodo <claudiobidau@gmail.com>
2016-09-16 20:55:24 -0300

    tokko labs como sponsor

--- pinaxcon/forms.py
+++ pinaxcon/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from captcha.fields import CaptchaField
-# our new form
+
 class BecasForm(forms.Form):
     nombre_completo = forms.CharField(required=True)
     email = forms.EmailField(required=True)
